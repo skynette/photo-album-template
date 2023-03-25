ddd65c8443a371dc67f87c811cddd08674747cf4
Joshua <cutejosh2@gmail.com>
2023-03-25 07:59:11 +0100

    start learning MUI

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { createRoot } from 'react-dom/client'
+
+function App() {
+	return (
+		<div>
+			MUI start
+		</div>
+	);
+}
+
+
+createRoot(document.getElementById('root')).render(<App />);
