5418d1834e263e3f8e0c50452a8c6d37c478e070
Joshua <cutejosh2@gmail.com>
2023-03-27 01:53:59 +0100

    adjusted style, added footer section

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@mui/material'
 import { PhotoCamera } from '@mui/icons-material'
-import { makeStyles } from '@mui/styles';
+import useStyles from './styles'
 
 
-const useStyles = makeStyles((theme) => ({
-	container: {
-		backgroundColor: "#FFF",
-	}
-}))
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 const App = () => {
 	const classes = useStyles()
@@ -17,30 +13,30 @@ const App = () => {
 			<CssBaseline />
 			<AppBar position="relative">
 				<Toolbar>
-					<PhotoCamera />
-					<Typography variant="h6" sx={{ ml: 1 }}>
+					<PhotoCamera className={classes.icon} />
+					<Typography variant="h6">
 						Photo Album
 					</Typography>
 				</Toolbar>
 			</AppBar>
 			<main>
-				<div style={{ padding: '28px 20px 26px' }} className={classes.container}>
+				<div className={classes.container}>
 					<Container maxWidth="sm" style={{ marginTop: '50px' }}>
 						<Typography variant='h2' align='center' color='textPrimary' gutterBottom>
 							Photo Album
 						</Typography>
 						<Typography variant='h5' align='center' color='textSecondary' paragraph>
-							Hello everyone this is a photo album and I am trying to make this long as posible to it can wrap around.....
+							Hello everyone this is a photo album and I am trying to make this long as posible to it can wrap around.
 						</Typography>
-						<div>
+						<div className={classes.buttons}>
 							<Grid container spacing={2} justifyContent="center">
 								<Grid item>
-									<Button variant="contained" color="primary" sx={{ marginTop: '40px' }}>
+									<Button variant="contained" color="primary">
 										See my photos
 									</Button>
 								</Grid>
 								<Grid item>
-									<Button variant="outlined" color="primary" sx={{ marginTop: '40px' }}>
+									<Button variant="outlined" color="primary">
 										Secondary Action
 									</Button>
 								</Grid>
@@ -48,37 +44,45 @@ const App = () => {
 						</div>
 					</Container>
 				</div>
-				<Container maxWidth="md">
+				<Container maxWidth="md" className={classes.cardGrid}>
 					<Grid container spacing={4}>
-						<Grid item sx={{ padding: '20px 0' }}>
-							<Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}>
-								<CardMedia
-									image="https://source.unsplash.com/random"
-									title="Image Title"
-									sx={{
-										paddingTop:
-											'56.25%'
-									}}
-								/>
-								<CardContent
-									sx={{ flexGrow: 1 }}
-								>
-									<Typography gutterBottom variant="h5">
-										Heading
-									</Typography>
-									<Typography>
-										This is a media card, use this for content description
-									</Typography>
-								</CardContent>
-								<CardActions>
-									<Button size="small" color="primary">View</Button>
-									<Button size="small" color="primary">Edit</Button>
-								</CardActions>
-							</Card>
-						</Grid>
+						{cards.map((card) => (
+							<Grid item key={card} xs={12} sm={6} md={4}>
+								<Card className={classes.card}>
+									<CardMedia
+										image="https://source.unsplash.com/random"
+										title="Image Title"
+										className={classes.cardMedia}
+									/>
+									<CardContent className={classes.cardContent}>
+										<Typography gutterBottom variant="h5">
+											Heading
+										</Typography>
+										<Typography>
+											This is a media card, use this for content description
+										</Typography>
+									</CardContent>
+									<CardActions>
+										<Button size="small" color="primary">View</Button>
+										<Button size="small" color="primary">Edit</Button>
+									</CardActions>
+								</Card>
+							</Grid>
+						))}
 					</Grid>
 				</Container>
+				<Container>
+
+				</Container>
 			</main>
+			<footer className={classes.footer}>
+				<Typography variant="h6" align="center" gutterBottom>
+					Footer
+				</Typography>
+				<Typography variant="subtitle1" align='center' color="textSecondary">
+					Something to give this footer a purpose
+				</Typography>
+			</footer>
 		</>
 	)
 }
--- src/styles.js
+++ src/styles.js
@@ -1,1 +1,35 @@
-// do the styles import here
+import { makeStyles } from '@mui/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+	container: {
+		backgroundColor: "#FFF",
+		padding: '28px 20px 26px',
+	},
+	icon: {
+		marginRight: '10px',
+	},
+	buttons: {
+		marginTop: '40px'
+	},
+	cardGrid: {
+		padding: '20px 0',
+	},
+	card: {
+		height: '100%',
+		display: 'flex',
+		flexDirection: 'column',
+	},
+	cardMedia: {
+		paddingTop: '56.25%',
+	},
+	cardContent: {
+		flexGrow: 1,
+	},
+	footer: {
+		backgroundColor: '#FFF',
+		padding: '50px 0'
+	}
+}))
+
+export default useStyles
